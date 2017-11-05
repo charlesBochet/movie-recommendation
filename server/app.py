eb244be8535454eaba8aef3e7de9d3e0c057456b
Charles BOCHET <charlesb@theodo.fr>
2017-11-05 23:01:31 +0100

    Add data

--- server/app.py
+++ server/app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/my-movies')
+def list_user_movies():
+    return render_template('my-movies.html', page_title='My Movies')
